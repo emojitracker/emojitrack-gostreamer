7f808950e0fbfacc1c7074ea2c81d55203b9fbb0
Matthew Rothenberg <mrothenberg@gmail.com>
2014-04-25 00:26:46 -0400

    might as well use a uint
    since we only increment.  maybe someone else will use this code for
    something that counts higher than 10 before being expunged lolz.

--- stream_summarizer.go
+++ stream_summarizer.go
@@ -5,32 +5,25 @@ import (
   "encoding/json"
 )
 
-/*type ScoreUpdate struct {
-  id string
-  score int
-}*/
-
 /****
  * receives a channel of string values, and counts their occurences.
  * every PERIOD duration, flush a *summary* of those values to output channel.
  ****/
- // TODO: change to uint?
 
 func ScorePacker(inputStream <-chan string, period time.Duration) <-chan []byte {
   outputStream := make(chan []byte)
   go func() {
-    scores := make(map[string]int)
+    scores := make(map[string]uint)
     ticker := time.Tick(period)
     for {
       select {
         case id := <-inputStream:
           //increment hash, since int nil-value is zero dont have to worry about init case!
-          //(could distribute workers with shared memory and atomic uint)
           scores[id] = scores[id] + 1
         case <-ticker:
           packedscores, _ := json.Marshal(scores)
           outputStream <- packedscores   // output to channel
-          scores = make(map[string]int)  // reset
+          scores = make(map[string]uint)  // reset
       }
     }
   }()
