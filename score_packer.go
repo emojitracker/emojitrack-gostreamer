d28c28aef4f83c56d25bf3a6a4d8f084a437217c
Matthew Rothenberg <mrothenberg@gmail.com>
2014-04-27 15:29:42 -0400

    cleanup docs and var names

--- score_packer.go
+++ score_packer.go
@@ -5,27 +5,27 @@ import (
   "encoding/json"
 )
 
-/****
- * receives a channel of string values, and counts their occurences.
- * every PERIOD duration, flush a *summary* of those values to output channel.
- ****/
-
-func ScorePacker(inputStream <-chan string, period time.Duration) <-chan []byte {
-  outputStream := make(chan []byte)
+// ScorePacker receives a channel of string values, and counts their occurences.
+//
+// Every period duration, it flushes a *summary* of those values to returned
+// output channel.  For now, this summary is returned as JSON, since that's how
+// I always end up using it.
+func ScorePacker(input <-chan string, period time.Duration) <-chan []byte {
+  output := make(chan []byte)
   go func() {
     scores := make(map[string]uint)
     ticker := time.Tick(period)
     for {
       select {
-        case id := <-inputStream:
-          //increment hash, since int nil-value is zero dont have to worry about init case!
-          scores[id] = scores[id] + 1
+        case id := <-input:
+          // since int nil-value is zero dont have to worry about init case!
+          scores[id] = scores[id] + 1    // increment hash
         case <-ticker:
           packedscores, _ := json.Marshal(scores)
-          outputStream <- packedscores   // output to channel
-          scores = make(map[string]uint)  // reset
+          output <- packedscores         // output to channel
+          scores = make(map[string]uint) // reset hash
       }
     }
   }()
-  return outputStream
+  return output
 }
